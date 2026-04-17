94b95f3e94e5334375647b9d83ac06a8814cd731
Marsh <holyfield444@gmail.com>
2026-04-17 10:58:22 +0100

    Variation manager

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 # Register your models here.
 @admin.register(Product)
@@ -8,3 +8,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('category', 'is_available')
     search_fields = ('product_name', 'description')
     prepopulated_fields = {'slug': ('product_name',)}
+
+
+@admin.register(Variation)
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date')
+    list_filter = ('product', 'variation_category', 'is_active')
+    list_editable = ('is_active',)
+    search_fields = ('product__product_name', 'variation_value')
